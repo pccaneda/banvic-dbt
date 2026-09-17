7dfabe5e823f17ece875be4b0c0cd1aac8135290
Pedro Caneda <cavalcantecaneda@gmail.com>
2026-09-17 19:06:05 +0000

    Added dimension colaboradores

---
+++ models/intermediate/int_dimensao_colaboradores.sql
@@ -0,0 +1,29 @@
+with
+    localidades as (
+        select *
+        from {{ ref('stg_erp__localidades') }}
+    ),
+
+    colaboradores as (
+        select *
+        from {{ ref('stg_erp__colaboradores') }}
+    ),
+
+    colaboradores_enriquecidos as (
+        select
+            colaboradores.pk_colaborador,
+            colaboradores.fk_gerente,
+            colaboradores.nome_colaborador,
+            colaboradores.email_colaborador,
+            colaboradores.cpf_colaborador,
+            colaboradores.data_nascimento_colaborador,
+            colaboradores.endereco_colaborador,
+            colaboradores.cep_colaborador,
+            localidades.cidade as cidade_colaborador,
+            localidades.uf as uf_colaborador
+        from colaboradores
+        left join localidades on colaboradores.fk_localidade = localidades.pk_localidade
+    )
+
+select *
+from colaboradores_enriquecidos
